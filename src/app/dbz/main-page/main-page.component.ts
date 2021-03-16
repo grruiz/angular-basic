7ae8d9b5a9818510c36083440e4ed0ee1ceea282
grruiz <galvezruizrafa@gmail.com>
2021-03-16 08:24:40 +0100

    Practice Angular 0.1

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Veggeta',
+      poder: 7000
+    }
+  ]
+
+
+  cambiarNombre(event: any) {
+    console.log(event.target.value);
+  }
+}
